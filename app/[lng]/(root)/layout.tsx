bcc80638f60d8ddd1bea5951f875da7ba71cecd2
OzodbekMuhtorjonov31 <ozodbekmuhtorjonov08@gmail.com>
2024-06-24 21:12:47 +0500

    Home page. UI

--- app/[lng]/(root)/layout.tsx
+++ app/[lng]/(root)/layout.tsx
@@ -1,12 +1,14 @@
 import { ChildProps } from '@/types'
 import React from 'react'
 import Navbar from './_components/navbar'
+import Footer from './_components/footer'
 
 function Layout({ children }: ChildProps) {
 	return (
 		<div>
 			<Navbar />
 			<main>{children}</main>
+			<Footer />
 		</div>
 	)
 }
