9b2774b4977cd131672bb74ce7494f4571990596
OzodbekMuhtorjonov31 <ozodbekmuhtorjonov08@gmail.com>
2024-06-25 12:45:47 +0500

    Webhooks

--- app/[lng]/instructor/(dashboard)/page.tsx
+++ app/[lng]/instructor/(dashboard)/page.tsx
@@ -27,13 +27,13 @@ function Page() {
 				description='Here are your latest courses'
 			/>
 
-			<div className='mt-4 grid grid-cols-3 gap-4'>
+			{/* <div className='mt-4 grid grid-cols-3 gap-4'>
 				{courses
 					.map(course => (
 						<InstructorCourseCard key={course.title} {...course} />
 					))
 					.slice(0, 3)}
-			</div>
+			</div> */}
 
 			<Header title='Reviews' description='Here are your latest reviews' />
 
